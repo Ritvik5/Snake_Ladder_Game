0e5a126999295ae2bcedc86bb7383e99bbb0bee1
Ritvik5 <sharma.ritvik56@gmail.com>
2023-02-20 19:46:56 +0530

    Using Random Function to roll the Die

--- SnakeLadderGame/Program.cs
+++ SnakeLadderGame/Program.cs
@@ -10,6 +10,9 @@
             int playerOne;
             playerOne = position;
             Console.WriteLine("Position of the playerOne is " +playerOne);
+            Random random = new Random();
+            int diceCheck = random.Next(0, 7);
+            Console.WriteLine("Rolled Dice number is " +diceCheck);
         }
     }
 }
