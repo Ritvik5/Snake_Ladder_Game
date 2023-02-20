610c1007d310d13ce954f503d5ad32153a04cd9c
Ritvik5 <sharma.ritvik56@gmail.com>
2023-02-20 19:37:32 +0530

    PlayerOne Position

---
+++ SnakeLadderGame/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace SnakeLadderGame
+{
+    internal class SnakeLadder
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Snake and Ladder Game");
+
+            int position = 0 ;
+            int playerOne;
+            playerOne = position;
+            Console.WriteLine("Position of the playerOne is " +playerOne);
+        }
+    }
+}
